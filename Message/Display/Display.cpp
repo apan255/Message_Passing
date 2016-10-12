14849039df7c0284f997ef1886e2928757d713b2
Aditya Pandey <apandey6@ncsu.edu>
2016-10-12 11:54:06 -0700

    Update Display.cpp

--- Message/Display/Display.cpp
+++ Message/Display/Display.cpp
@@ -1,11 +1,7 @@
 /////////////////////////////////////////////////////////////////////
 //  Display.cpp - Display Message Passing Project                  //
 //                                                                 //
-//  ver 1.0                                                        //
-//  Language:      Visual C++ 2013                                 //
-//  Platform:      Dell, Windows 8.1                               //
-//  Application:   Scope analysis, OOD Assignment 3, Spring2015    //
-//  Author:		   Ankur Pandey                                    //
+
 /////////////////////////////////////////////////////////////////////
 
 #include <iostream>

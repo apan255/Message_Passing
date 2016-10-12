8375ed9c18a7e21f435a2b1d10c0624a780bebbe
Aditya Pandey <apandey6@ncsu.edu>
2016-10-12 11:51:08 -0700

    Update Reciever.cpp

--- Message/Reciever/Reciever.cpp
+++ Message/Reciever/Reciever.cpp
@@ -1,11 +1,7 @@
 /////////////////////////////////////////////////////////////////////
 //  Reciever.cpp -  Recieves Information end point in blocking call//
 //                And send back to client                          //
-//  ver 1.0                                                        //
-//  Language:      Visual C++ 2013                                 //
-//  Platform:      Dell, Windows 8.1                               //
-//  Application:   Scope analysis, OOD Assignment 3, Spring2015    //
-//  Author:		   Ankur Pandey                                    //
+
 /////////////////////////////////////////////////////////////////////
 #include <iostream>
 #include "Reciever.h"

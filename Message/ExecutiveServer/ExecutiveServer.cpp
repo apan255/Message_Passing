95bff0d132a4f377f8ad73c4938d9053298ae689
Aditya Pandey <apandey6@ncsu.edu>
2016-10-12 11:52:57 -0700

    Update ExecutiveServer.cpp

--- Message/ExecutiveServer/ExecutiveServer.cpp
+++ Message/ExecutiveServer/ExecutiveServer.cpp
@@ -2,10 +2,7 @@
 //  ExecutiveServer.cpp - Execute Server side                      //
 //                                                                 //
 //  ver 1.0                                                        //
-//  Language:      Visual C++ 2013                                 //
-//  Platform:      Dell, Windows 8.1                               //
-//  Application:   Scope analysis, OOD Assignment 3, Spring2015    //
-//  Author:		   Ankur Pandey                                    //
+
 /////////////////////////////////////////////////////////////////////
 #include "ExecutiveServer.h"
 

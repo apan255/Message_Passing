ef4aa3b725696888efcd6653a4b9fd4025a61ecc
Aditya Pandey <apandey6@ncsu.edu>
2016-10-12 11:52:06 -0700

    Update MsgHeader.h

--- Message/MsgHeader/MsgHeader.h
+++ Message/MsgHeader/MsgHeader.h
@@ -4,10 +4,7 @@
 //  MsgHeader.h - Conatin Message Header Information               //
 //                                                                 //
 //  ver 1.0                                                        //
-//  Language:      Visual C++ 2013                                 //
-//  Platform:      Dell, Windows 8.1                               //
-//  Application:   Scope analysis, OOD Assignment 3, Spring2015    //
-//  Author:		   Ankur Pandey                                    //
+
 /////////////////////////////////////////////////////////////////////
 /*
 Module Operations:
